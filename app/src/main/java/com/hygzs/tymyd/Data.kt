3c5da3cca3d184cd7f2e069ef30c2cb9e8c44bc9
JoeLeaf <1341806518@qq.com>
2023-12-30 00:06:22 +0800

    修改了gzip解密文件名,新增台服支持。更新了版本哦

--- app/src/main/java/com/hygzs/tymyd/Data.kt
+++ app/src/main/java/com/hygzs/tymyd/Data.kt
@@ -8,7 +8,9 @@ package com.hygzs.tymyd
 
 object Data {
     var app = "com.tencent.tmgp.wuxia"
+    var apptf = "com.garena.game.mbmtw"
     var PathName = "com.tencent.tmgp.wuxia/files"
+    var PathNametf = "com.garena.game.mbmtw/files"
     var TargetAccount = ""
     var TargetFriend = ""
     var msg = ""
--- app/src/main/java/com/hygzs/tymyd/MainActivity.java
+++ app/src/main/java/com/hygzs/tymyd/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.blankj.utilcode.util.SPUtils;
+import com.blankj.utilcode.util.ToastUtils;
+import com.hss01248.dialog.StyledDialog;
+import com.hss01248.dialog.interfaces.MyDialogListener;
 import com.hygzs.tymyd.customView.PathAnimTextView;
 import com.hygzs.tymyd.ui.Agreement;
 import com.hygzs.tymyd.ui.ChatRecords;
@@ -26,15 +29,58 @@ public class MainActivity extends BaseActivity {
                 Thread.sleep(4000);
                 runOnUiThread(() -> {
                     if (SPUtils.getInstance("config").contains("isAgree")) {
+                        if (SPUtils.getInstance("config").getString("channel").equals("tf")) {
+                            Data.INSTANCE.setPathName(Data.INSTANCE.getPathNametf());
+                            Data.INSTANCE.setApp(Data.INSTANCE.getApptf());
+                        }
                         Intent i = new Intent(this, ChatRecords.class);
                         startActivity(i, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
                         finish();
                     } else {
-                        Intent i = new Intent(this, Agreement.class);
-                        startActivity(i, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
-                        finish();
-                    }
+                        if (SPUtils.getInstance("config").contains("channel")){
+                            if (SPUtils.getInstance("config").getString("channel").equals("tf")) {
+                                Data.INSTANCE.setPathName(Data.INSTANCE.getPathNametf());
+                                Data.INSTANCE.setApp(Data.INSTANCE.getApptf());
+                            }
+                            Intent i = new Intent(this, Agreement.class);
+                            startActivity(i, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+                            finish();
+                        }else {
+                            //这里做国服或者台服的选择弹窗
+                            StyledDialog.buildIosAlert("小叶子提示", "请问你是国服还是台服版？",
+                                    new MyDialogListener() {
+                                        @Override
+                                        public void onThird() {
+                                            //台服
+                                            SPUtils.getInstance("config").put("channel","tf");
+                                            Data.INSTANCE.setPathName(Data.INSTANCE.getPathNametf());
+                                            Data.INSTANCE.setApp(Data.INSTANCE.getApptf());
+                                            runOnUiThread(() -> {
+                                                Intent i = new Intent(MainActivity.this, Agreement.class);
+                                                startActivity(i, ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
+                                                finish();
+                                            });
+                                        }
 
+                                        @Override
+                                        public void onFirst() {
+                                            //国服
+                                            SPUtils.getInstance("config").put("channel","gf");
+                                            runOnUiThread(() -> {
+                                                Intent i = new Intent(MainActivity.this, Agreement.class);
+                                                startActivity(i, ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
+                                                finish();
+                                            });
+                                        }
+                                        @Override
+                                        public void onSecond() {
+                                            ToastUtils.showLong("。。。这都不知道啊。。。那再见！");
+                                            finish();
+                                        }
+                                    }
+                            ).setBtnText("国服","不知道","台服").show();
+                        }
+                    }
                 });
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
